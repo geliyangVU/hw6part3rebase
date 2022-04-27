a5fc456717635ba6a8b7e0cd56f253b666220e0b
Geli Yang <gyang5@villanova.edu>
2022-04-27 12:17:35 -0400

    add favorite album from merge-sandbox branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from main branch");
         System.out.println("My favorite musician is Eminem");
+        System.out.println("My favorite album is Recovery");
 
     }
 
